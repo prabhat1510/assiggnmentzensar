242eee5d77266c791edaa71a92adc21f26f6af6c
Subbarao9133 <103561230+Subbarao9133@users.noreply.github.com>
2022-05-17 06:47:58 +0530

    Completed

---
+++ Subbarao_assignment/src/main/java/com/training/demo/AssignmentspringApplication.java
@@ -0,0 +1,13 @@
+package com.training.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentspringApplication.class, args);
+	}
+
+}
