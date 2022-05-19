8909b73ffc425e22b9b35a715009e5f9d59b5bea
Vivek Ambetkar <v.ambetkar@zensar.com>
2022-05-19 10:52:42 +0530

    Done

---
+++ vivek1/OLX-Login/src/main/java/com/training/olxlogin/entities/Users.java
@@ -0,0 +1,98 @@
+package com.training.olxlogin.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 20)
+	private String username;
+	
+	@Column(nullable = false, length = 20)
+	private String password;
+	
+	@Column(nullable = false)
+	private Integer phone;
+	
+	@Column(nullable = false)
+	private boolean active;
+	
+	@Column(nullable = false, length = 45)
+	private String firstname;
+	
+	@Column(nullable = false, length = 45)
+	private String lastname;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", username=" + username + ", password=" + password + ", phone=" + phone
+				+ ", active=" + active + ", firstname=" + firstname + ", lastname=" + lastname + "]";
+	}
+	
+	
+}
