c87bf6572af930a0a244d4b16a3efe5e89b6984c
Lakshmanarao55 <103560756+Lakshmanarao55@users.noreply.github.com>
2022-06-20 19:45:42 +0530

    OLX Done

---
+++ Lakshmanarao/olxmasterdata/olxmasterdata/src/main/java/com/training/olxmasterdata/service/OlxMasterdataService.java
@@ -0,0 +1,17 @@
+package com.training.olxmasterdata.service;
+
+import java.util.List;
+
+import com.training.olxmasterdata.entities.AdvStatus;
+import com.training.olxmasterdata.entities.Categories;
+
+public interface OlxMasterdataService {
+
+	public List<AdvStatus> getAllAdvStatus();
+
+	public List<Categories> getAllCategories();
+
+	public void addAdvertiseStatus();
+
+	public void addCategories();
+}
